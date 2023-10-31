068fdceb347bfdcb211601db0a7b200dcd5912a3
Krish Vijay Ramani <136609165+KrishRamani2@users.noreply.github.com>
2023-10-31 21:31:10 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Header = () => {
+  return (
+    <>
+      <div className='header'>
+        <h1>Keep</h1>
+      </div>
+    </>
+  );
+  
+};
+
+export default Header;
